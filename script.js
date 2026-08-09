9d53b4346a58823003f8782ede09d866b414dfe8
sanareveritas <sanareveritas@gmail.com>
2026-08-09 11:19:09 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll(".card, .card-large");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
