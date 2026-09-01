18a418e38c1528ea456c50e79fc6c410460c1dcc
Maximillian Eldrich Zhu <eldrichzhu@gmail.com>
2026-09-01 11:47:19 +0700

    fixed padding issues, testing automatic header loader.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,17 @@ const observer = new IntersectionObserver(
 cards.forEach((card) => {
     observer.observe(card);
 });
+
+function loadHeader() {
+    const path = window.location.pathname;
+    const isID = path.includes('/id/');
+    const headerFile = isID ? 'header-id.html' : 'header-en.html';
+
+    fetch('../assets/includes/${headerFile}')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        })
+}
+
+document.addEventListener('DOMContentLoaded', loadHeader);
